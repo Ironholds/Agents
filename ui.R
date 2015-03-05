afb1b75638be88573701c61eea9685317616eb38
ironholds <ironholds@gmail.com>
2015-03-05 18:42:20 -0500

    tweaks

--- ui.R
+++ ui.R
@@ -19,18 +19,20 @@ shinyUI(fluidPage(
                   selected = "readers"),
       h2("About this data"),
       HTML("This data contains the most common user agents for Wikimedia readers and editors.
-           Agents are provided parsed using the open-source <a href = 'http://www.uaparser.org/'>ua-parser</a> project."),
+            Agents are provided parsed using the open-source <a href = 'http://www.uaparser.org/'>ua-parser</a> project."),
       h3("Details"),
       HTML("The intent
            behind releasing the parsed agents is to make it easier for Wikimedia developers
-           to understand how to best test their software for the group they're targeting. We are
-           looking into potentially releasing the <em>raw</em> agents as well, in order to enable
-           upstream developers to refine user agent parsers.<br/><br/>"),
+           to understand how to best test their software for the group they're targeting.<br/><br/>"),
       htmlOutput("links"),
       h2("Reusing this data"),
       HTML("The data is released into the public domain under the
            <a href = 'https://creativecommons.org/publicdomain/zero/1.0/'>CC-0 public domain dedication</a>, and can be freely reused
-           by all and sundry.")
+           by all and sundry. Iff you decide you want to credit it to people, though, the appropriate citation is:
+           <br/><br/>
+           <blockquote><a href = 'http://blog.ironholds.org/'>Keyes, Oliver</a> (2015)
+              <em><a href = 'http://dx.doi.org/10.6084/m9.figshare.1326739'>Browser Choices of Wikimedia Readers and Editors</a></em>
+           </blockquote>")
     ),
 
     # Show a plot of the generated distribution
